84ad1512cd6759cf11af6d6023eb07a79a92b189
belair34 <belair.34@osu.edu>
2020-01-31 13:32:43 -0500

    Cleared out sprint 0 stuff

--- Game1/Game1.cs
+++ Game1/Game1.cs
@@ -7,13 +7,10 @@ namespace Game1
 {
     public class Game1 : Game
     {
-        public Texture2D luigi;
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         SpriteFont text;
         List<IController> controllers;
-        ISprite playerSprite;
-        Vector2 playerPosition;
 
         public Game1()
         {
@@ -21,24 +18,9 @@ namespace Game1
             Content.RootDirectory = "Content";
         }
 
-        public Vector2 playerPos
-        {
-            get { return playerPosition; }
-        }
-        public void SetPlayerPos(int x, int y)
-        {
-            playerPosition.X = x;
-            playerPosition.Y = y;
-        }
-
-        public void SetPlayerSprite(ISprite sprite)
-        {
-            playerSprite = sprite;
-        }
+ 
         protected override void Initialize()
         {
-            playerPosition.X = 350;
-            playerPosition.Y = 200;
             controllers = new List<IController>();
             controllers.Add(new KeyboardController(this));
             controllers.Add(new MouseController(this));
@@ -49,9 +31,7 @@ namespace Game1
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            luigi = Content.Load<Texture2D>("luigi");
-            text = Content.Load<SpriteFont>("font");
-            playerSprite = new SpriteNMNA(this);
+
         }
 
    
@@ -66,17 +46,14 @@ namespace Game1
             {
                 controller.Update();
             }
-            playerSprite.Update();
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            playerSprite.Draw(spriteBatch);
-            spriteBatch.Begin();
-            spriteBatch.DrawString(text, "Credits\nProgram by: Mike Belair\nSprites from: http://www.mariouniverse.com/sprites/nes/",new Vector2(100,400), Color.White);
-            spriteBatch.End();
+
             base.Draw(gameTime);
         }
     }
