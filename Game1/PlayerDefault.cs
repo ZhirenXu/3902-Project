ba9f3ed0c9ecb9366444a6a9ee508b28799a39aa
belair34 <belair.34@osu.edu>
2020-02-04 20:08:39 -0500

    Basic Player class, Shell PlayerState interface

---
+++ Game1/PlayerDefault.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+	public class PlayerDefault : IPlayer
+	{
+		private Vector2 position;
+		private IPlayerState state;
+		public PlayerDefault(int x, int y, IPlayerState state)
+		{
+			this.position.X = x;
+			this.position.Y = y;
+			this.state = state;
+		}
+
+		public void setPosition(int x, int y)
+		{
+			this.position.X = x;
+			this.position.Y = y;
+		}
+
+		public Vector2 getPosition()
+		{
+			return this.position;
+		}
+
+		public void changeState(IPlayerState state)
+		{
+			this.state = state;
+		}
+
+	}
+}
