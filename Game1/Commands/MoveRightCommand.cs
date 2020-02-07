9188f59ab739460beae64e1fd3af5c8a6a37642f
armstrong-775 <armstrong.775@osu.edu>
2020-02-06 21:44:47 -0500

    Added more commands

---
+++ Game1/Commands/MoveRightCommand.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+    class MoveRightCommand : ICommand
+    {
+		private Game1 myGame;
+
+		public MoveRightCommand(Game1 game)
+		{
+			myGame = game;
+		}
+
+		public void Execute()
+		{
+			myGame.player.MoveRight();
+		}
+	}
+}
