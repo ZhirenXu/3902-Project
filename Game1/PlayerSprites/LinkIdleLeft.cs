54a24fc1d00252a3c4bf8478f45d61be65ae182c
Zhiren Xu <xu.3579@buckeyemail.osu.edu>
2020-02-07 20:15:34 -0500

    Add sprites in idle.

---
+++ Game1/PlayerSprites/LinkIdleLeft.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1.PlayerSprites
+{
+    class LinkIdleLeft :ISprite
+    {
+        Texture2D texture;
+        IPlayer player;
+        int srcWidth = 15;
+        int srcHeight = 16;
+        int destWidth = 30;
+        int destHeight = 32;
+        int srcX = 30; /*Change this*/
+        int srcY = 30;  /*and this*/
+        int curFrame = 1;
+        int totalFrames = 2; /*Maybe this*/
+        int delay = 0;     
+
+        public LinkIdleLeft(IPlayer player, Texture2D texture)
+        {
+            this.texture = texture;
+            this.player = player;
+        }
+        public void Update()
+        {
+            delay++;
+            if(delay == 7) /*Delay of frame changes*/
+            {
+                delay = 0;
+                curFrame++;
+                if(curFrame > totalFrames)
+                {
+                    curFrame = 1;
+                }
+            }
+            player.SetPosition((int)player.GetPosition().X, (int)player.GetPosition().Y); /*Change this*/
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Rectangle destRec = new Rectangle((int)player.GetPosition().X, (int)player.GetPosition().Y, destWidth, destHeight);
+            Rectangle srcRec;
+            srcRec = new Rectangle(srcX, srcY, srcWidth, srcHeight);
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture, destRec, srcRec, Color.White);
+            spriteBatch.End();
+        }
+    }
+}
