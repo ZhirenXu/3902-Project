733f62d6ed582fecc0e695a71c09dc176b093a56
armstrong-775 <armstrong.775@osu.edu>
2020-02-12 13:14:42 -0500

    Added candle

---
+++ Game1/Commands/BlueCandleCommand.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+    class BlueCandleCommand : ICommand
+    {
+		private Game1 myGame;
+
+		public BlueCandleCommand(Game1 game)
+		{
+			myGame = game;
+		}
+
+		public void Execute()
+		{
+			//if already used in screen, do nothing & end command
+			//create fire projectile
+			//if room is dark, light room
+			myGame.GetPlayer().SlotB();
+		}
+	}
+}
