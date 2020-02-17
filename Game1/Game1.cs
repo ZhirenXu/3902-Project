3f17b48880a62ba002a07823bf325ca5dac3ccea
Xian Zhang <zhang.6757@buckeyemail.osu.edu>
2020-02-17 16:18:59 -0500

    Should be a working version now

--- Game1/Game1.cs
+++ Game1/Game1.cs
@@ -9,7 +9,6 @@ namespace Game1
 {
     public class Game1 : Game
     {
-        //THis is Simon first comment on git
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Rectangle backgroundSrcRec;
@@ -18,7 +17,6 @@ namespace Game1
         List<IController> controllers;
         IPlayer player;
         IEnemy enemy;
-        //IItem item;
         private Texture2D background;
 
         public Game1()
@@ -31,10 +29,6 @@ namespace Game1
         {
             return this.player;
         }
-        /*public IItem GetItem()
-        {
-            return this.item;
-        }*/
         public IEnemy GetEnemy()
         {
             return this.enemy;
@@ -46,7 +40,6 @@ namespace Game1
             SpriteFactory.Instance.LoadAll(Content);
             player = new PlayerDefault(100, 100, 6, 6, GraphicsDevice);
             enemy = new EnemyDefault(600, 300, 3, 6, GraphicsDevice);
-            //item = new ItemDefault(600, 200, GraphicsDevice);
             this.backgroundSrcRec = new Rectangle(257, 0, 256, 176);
             this.backgroundDestRec = new Rectangle(0, 0, spriteBatch.GraphicsDevice.Viewport.Width, spriteBatch.GraphicsDevice.Viewport.Height);
             controllers = new List<IController>();           /*Controllers*/
